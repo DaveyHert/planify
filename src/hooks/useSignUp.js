85e03446397dfd280301c954112c8a12c936ff4d
daveyhert <daveyhert@gmail.com>
2025-04-03 18:56:49 +0100

    add user document creation in Firestore during sign-up process

--- src/hooks/useSignUp.js
+++ src/hooks/useSignUp.js
@@ -3,6 +3,7 @@ import { firebaseAuth, firebaseStorage, firestoreDB } from "../firebase/config";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { useAuthContext } from "./useAuthContext";
+import { collection, setDoc } from "firebase/firestore";
 
 export function useSignUp() {
   const [isPending, setIsPending] = useState(false);
@@ -11,7 +12,6 @@ export function useSignUp() {
   const { dispatch } = useAuthContext();
 
   const signUpNewUser = async (email, password, displayName, avatar) => {
-    let isMounted = true; // Prevent state updates if unmounted
     setError(null);
     setIsPending(true);
     setResponse(null);
@@ -25,36 +25,28 @@ export function useSignUp() {
       );
       if (!res) throw new Error("Could not complete signup");
 
-      // upload users avater/profile pics
+      // upload users avatar/profile pics
       const uploadPath = `thumnbnails/${res.user.uid}/${avatar.name}`;
       const storageRef = ref(firebaseStorage, uploadPath);
       const snapshot = await uploadBytes(storageRef, avatar);
+
+      //get avatar url and update new user's profile with display name and photo url
       if (!snapshot) throw new Error("Failed to upload profile image");
-
-      //get avatar url
       const avatarURL = await getDownloadURL(storageRef);
-      console.log(avatarURL);
-
-      // update new user with display name
       await updateProfile(res.user, { displayName, photoURL: avatarURL });
-      console.log(res);
-      if (isMounted) {
-        dispatch({ type: "SIGN_UP", payload: res.user });
-        setIsPending(false);
-        setResponse(res);
-      }
+
+      // create a user document for the user with their unique ID
+      const docRef = collection(firestoreDB, "users", res.user.uid);
+      await setDoc(docRef, { online: true, displayName, photoURL: avatarURL });
+
+      dispatch({ type: "SIGN_UP", payload: res.user });
+      setIsPending(false);
+      setResponse(res);
     } catch (err) {
       console.error("Signup error:", err); // ✅ Debugging
-      if (isMounted) {
-        setError(err.message);
-        setIsPending(false);
-      }
+      setError(err.message);
+      setIsPending(false);
     }
-
-    // Cleanup function to prevent memory leaks
-    return () => {
-      isMounted = false;
-    };
   };
   return { error, isPending, signUpNewUser, response };
 }

2922bcb6977ff4f6f86dc30df318595890b3e2bd
daveyhert <daveyhert@gmail.com>
2025-04-03 22:42:08 +0100

    feat: enhance Avatar component with wrapper for improved layout and styling

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,7 +3,9 @@ import "./Avatar.css";
 export default function Avatar({ user }) {
   return (
     <div className='user'>
-      <img className='avatar' src={user?.photoURL} alt="user's avatar" />
+      <div className='avatar-wrapper'>
+        <img className='avatar' src={user?.photoURL} alt="user's avatar" />
+      </div>
       <p>Hey {user?.displayName}</p>
     </div>
   );

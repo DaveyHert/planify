ad7481d4fa59127aaf30d5abea4f047602196a84
daveyhert <daveyhert@gmail.com>
2025-04-14 08:00:33 +0100

    feat: enhance ProjectSummary with completion status display and update button functionality; add new CSS styles for project summary status

--- src/pages/project/ProjectSummary.jsx
+++ src/pages/project/ProjectSummary.jsx
@@ -2,43 +2,61 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 import "./Project.css";
 import Avatar from "../../components/Avatar";
 import LabelIcon from "../../assets/label-icon.svg";
-
+import { useUpdateDocument } from "../../hooks/useUpdateDocument";
+import ProgressIcon from "../../components/ProgressIcon";
 export default function ProjectSummary({ project }) {
   const { user } = useAuthContext();
+  const { updateItem, isPending, error } = useUpdateDocument("projects");
 
+  const handleComplete = async () => {
+    await updateItem(project.id, {
+      status: "completed",
+    });
+  };
   return (
     <div>
-      <div className='project-summary'>
-        <h2 className='page-title'>{project.name}</h2>
-        <div className='project-info'>
-          <p className='due-date'>
+      <div className="project-summary">
+        <h2 className="page-title">{project.name}</h2>
+        <div className="project-info">
+          <p className="due-date">
             Due date<span>{project.dueDate.toDate().toDateString()}</span>
           </p>
 
           <div
-            className='created-by'
+            className="created-by"
             data-user-name={project.createdBy.displayName}
           >
             Created by <Avatar src={project.createdBy.photoURL} />
           </div>
-          <div className='label'>
-            <img src={LabelIcon} alt='label' />
+          <div className="label">
+            <img src={LabelIcon} alt="label" />
             <span>{project.category}</span>
           </div>
         </div>
-        <p className='details'>{project.details}</p>
+        <p className="details">{project.details}</p>
         <h4>Project assigned to:</h4>
-        <div className='assigned-users'>
+        <div className="assigned-users">
           {project.assignedUsers.map((user) => (
             <div key={user.id} data-user-name={user.displayName}>
               <Avatar src={user.photoURL} />
             </div>
           ))}
         </div>
+        <p className="status">
+          Status: {project.status === "completed" ? "Completed" : "Ongoing"}
+        </p>
       </div>
       {user.uid === project.createdBy.id && (
-        <button className='btn'>Mark as Complete</button>
+        <button
+          className="btn"
+          disabled={project.status}
+          onClick={handleComplete}
+        >
+          {project.status ? "Completed" : "Mark as Complete"}
+        </button>
       )}
+      {isPending && <ProgressIcon />}
+      {error && <p className="error">{error}</p>}
     </div>
   );
 }
